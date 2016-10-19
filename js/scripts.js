690658d7cf13d8d7757e1f8b1372fdab8dbf2f23
Eric Bomblatus and Michelle Llaguno <eric@>
2016-10-19 09:29:43 -0700

    create simple form with a letter output

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $("#blanks form").submit(function(event) {
+    var name1Input = $("input#name1").val();
+    $(".name1").text(name1Input);
+    $("#letter").show();
+    event.preventDefault();
+  });
+});
