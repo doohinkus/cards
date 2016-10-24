4ad43e7044f252a9ff1ab06c4b55b5cf5f70c0cd
Michelle Llaguno and Rafael Perez <michelle@>
2016-10-24 12:02:36 -0700

    capitalize items in list

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,25 @@
 $(document).ready(function() {
-  $("#blanks form").submit(function(event) {
-   var inputs =["name", "adjective", "noun"];
+  $("form.form-inline").submit(function(event) {
 
+   var inputs =["item1", "item2", "item3"];
+   var list = [];
+
+console.log($("#item1").val());
    inputs.forEach(function(input) {
      var value = $("#"+input).val();
-     $("."+input).text(value);
-   })
-    $("#letter").show();
+     list.push(value);
+     console.log(value);
+   });
+   var upperCaseList = list.map(function(item){
+     return item.toUpperCase();
+   });
+   upperCaseList = upperCaseList.sort();
+   console.log(upperCaseList);
+   upperCaseList.forEach(function (listItem){
+     $("ul").append("<li>" + listItem + "</li>");
+   });
+
+    $("#finallist").show();
     event.preventDefault();
   });
 });
